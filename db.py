cc5fd17fb968372a4bf0e6acc94f3d76ff3993d6
Eldar <35378787+eldarbr@users.noreply.github.com>
2021-11-18 02:28:52 +0300

    Update db.py

--- db.py
+++ db.py
@@ -7,48 +7,51 @@ class Databaser:
     connection = psycopg2.connect(config.database())
     cursor = connection.cursor()
 
-    def insert_photo(self, photo_href, name, description, timestamp, hidden=False, categories=None):
+    def insert_photo(self, photo_href, photo_name, photo_description, timestamp, hidden=False, photo_categories=None):
         """
         Inserts photo to database
         :param photo_href: href to CDN
-        :param name: name of photo
-        :param description: description of photo
+        :param photo_name: name of photo
+        :param photo_description: description of photo
         :param timestamp: when photo was taken
         :param hidden: if the photo is hidden; default-False
-        :param categories: list of categories that should be assigned
-        :return: number of errors
+        :param photo_categories: list of categories that should be assigned
+        :return: -1 if insertion failed; else photo id, number of errors
         """
         to_return = 0
-        if categories is None:
-            categories = []
+        if photo_categories is None:
+            photo_categories = []
         cursor = self.cursor
         try:
             cursor.execute("INSERT INTO photos (name, description, date_taken, hidden, href)"
                            "VALUES (%s, %s, %s, %s, %s)"
-                           "RETURNING photo_id", (name, description, timestamp, hidden, photo_href))
+                           "RETURNING photo_id", (photo_name, photo_description, timestamp, hidden, photo_href))
             self.connection.commit()
         except Exception as e:
+            self.connection.rollback()
+            print("ERROR - conflict while inserting photo {} into photos".format(photo_name))
             print(e)
+            return -1,
         photo_id = cursor.fetchall()[0][0]
-        to_return += self.assign_photo_photo_categories(photo_id, categories)
-        return to_return
+        to_return += self.assign_photo_photo_categories(photo_id, photo_categories)
+        return photo_id, to_return
 
-    def modify_photo(self, photo_id, photo_href=None, name=None, description=None, timestamp=None, hidden=None,
-                     categories=None):
+    def modify_photo(self, photo_id, photo_href=None, photo_name=None, photo_description=None,
+                     timestamp=None, hidden=None, photo_categories=None):
         """
         Replaces entry at photos table with specified data maintaining photo id
         Re-assigns categories (initiates photos_categories table modify)
         :param photo_id: id of photo to be modified
         :param photo_href: href to CDN
-        :param name: name of photo
-        :param description: description of photo
+        :param photo_name: name of photo
+        :param photo_description: description of photo
         :param timestamp: when photo was taken
         :param hidden: if the photo is hidden; default-False
-        :param categories: list of categories that should be assigned
+        :param photo_categories: list of categories that should be assigned
         :return: -1 - no id entry found; -2 - update error; number of errors
         """
         cursor = self.cursor
-        new_values = (name, description, timestamp, hidden, photo_href)
+        new_values = (photo_name, photo_description, timestamp, hidden, photo_href)
         cursor.execute("SELECT * FROM photos WHERE photo_id=%s", photo_id)
         result = cursor.fetchall()
         if len(result) == 0:
@@ -63,16 +66,16 @@ class Databaser:
                                "WHERE photo_id=%s", values + (photo_id,))
                 self.connection.commit()
             except Exception as e:
-                print(e)
                 self.connection.rollback()
+                print("ERROR modifying photo id {}".format(photo_id))
+                print(e)
                 return -2
-
-        if categories is not None:
-            return self.modify_photo_photo_categories(photo_id, categories)
+        if photo_categories is not None:
+            return self.modify_photo_photo_categories(photo_id, photo_categories)
 
     def assign_photo_photo_categories(self, photo_id, category_ids):
         """
-        Creates assignation of the photo id with the category ids
+        Creates assignment of the photo id with the category ids
         :return: number of errors
         """
         to_return = 0
@@ -83,10 +86,10 @@ class Databaser:
                     cursor.execute("INSERT INTO photo_categories VALUES (%s, %s)", (photo_id, category_id))
                     self.connection.commit()
                 except Exception as e:
-                    print("WARN - conflict while inserting into photo_categories values {} {}".
+                    self.connection.rollback()
+                    print("ERROR - conflict while inserting into photo_categories values {} {}".
                           format(photo_id, category_id))
                     print(e)
-                    self.connection.rollback()
                     to_return += 1
         return to_return
 
@@ -111,14 +114,57 @@ class Databaser:
             cursor.execute("DELETE FROM photo_categories WHERE photo_id=%s", photo_id)
             self.connection.commit()
         except Exception as e:
+            self.connection.rollback()
             print(e)
             return 1
         return 0
 
-    def insert_category(self):
-        pass
+    def insert_category(self, category_name, category_description, hidden, category_photos=None):
+        cursor = self.cursor
+        if category_photos is None:
+            category_photos = []
+        to_return = 0
+        try:
+            cursor.execute("INSERT INTO categories (name, description, hidden)"
+                           "VALUES (%s, %s, %s)"
+                           "RETURNING category_id", (category_name, category_description, hidden))
+            self.connection.commit()
+        except Exception as e:
+            self.connection.rollback()
+            print("ERROR - conflict while inserting category {} into categories".format(category_name))
+            print(e)
+            return -1,
+        category_id = cursor.fetchall()[0][0]
+        to_return += self.assign_category_photo_categories(category_id, category_photos)
+        return category_id, to_return
 
     def modify_category(self):
+        pass
+
+    def assign_category_photo_categories(self, category_id, photo_ids):
+        """
+        Creates assignment of the photo id with the category ids
+        :return: number of errors
+        """
+        to_return = 0
+        cursor = self.cursor
+        if len(photo_ids) > 0:
+            for photo_id in photo_ids:
+                try:
+                    cursor.execute("INSERT INTO photo_categories VALUES (%s, %s)", (photo_id, category_id))
+                    self.connection.commit()
+                except Exception as e:
+                    self.connection.rollback()
+                    print("ERROR - conflict while inserting values {} {} into photo_categories".
+                          format(photo_id, category_id))
+                    print(e)
+                    to_return += 1
+        return to_return
+
+    def modify_category_photo_categories(self, category_id, photo_ids):
+        pass
+
+    def delete_category_photo_categories(self, category_id):
         pass
 
     def get_photo(self, photo_id):
